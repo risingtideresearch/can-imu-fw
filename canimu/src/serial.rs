ecc9c33f72a6d2a4fe2caf7b357c54abc113f392
Jeff McBride <jeff@jeffmcbride.net>
2026-02-25 16:17:49 -0800

    Make serial string static

--- canimu/src/serial.rs
+++ canimu/src/serial.rs
@@ -2,6 +2,7 @@ use core::{fmt::Write as _, hash::Hasher as _};
 
 use hash32::FnvHasher;
 use heapless::String;
+use lazy_static::lazy_static;
 use stm32_metapac as pac;
 
 /// Compute a 32-bit unique serial number from the UID register value.
@@ -13,10 +14,16 @@ pub fn get_serial() -> u32 {
     ctx.finish() as u32
 }
 
+lazy_static! {
+    static ref SERIAL_STR: String<8> = {
+        let serial = get_serial();
+        let mut out = String::<8>::new();
+        let _ = write!(&mut out, "{serial:08X}");
+        out
+    };
+}
+
 /// Convert the 32-bit serial number to an 8-character uppercase hex string.
-pub fn get_serial_str() -> String<8> {
-    let serial = get_serial();
-    let mut out = String::<8>::new();
-    let _ = write!(&mut out, "{serial:08X}");
-    out
+pub fn get_serial_str() -> &'static str {
+    &SERIAL_STR
 }
